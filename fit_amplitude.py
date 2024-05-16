ba7adfbf07e4626dee00c850ece2cb9430390517
Gabriele Amato <amatogabry250@gmail.com>
2024-05-16 12:43:09 +0200

    Added reduced chi square, changed some graphics

--- fit_amplitude.py
+++ fit_amplitude.py
@@ -27,6 +27,12 @@ def main():
         A = (num / (r**2))**(-1/2)
         return v0 * A
 
+    def rcs(x, y , yerr, popt):
+        residuals = y - amplitude(x, *popt)
+        chi_squared = np.sum(((residuals/yerr)**2))
+        ndof = len(x) - len(popt)
+        return chi_squared / ndof
+
     f = 1 / np.sqrt(l * c) / 2 / np.pi # calcolo frequenze di notch e fattori di qualità
     q1 = r1 * c * 2 * np.pi * f
     q2 = r2 * c * 2 * np.pi * f
@@ -38,38 +44,39 @@ def main():
 
     x1,y1,yerr1 = np.loadtxt("r1_amp.txt", unpack=True)
     x2,y2,yerr2 = np.loadtxt("r2_amp.txt", unpack=True)
-    x3,y3,yerr3 = np.loadtxt("r3_amp.txt", unpack=True)
+#     x3,y3,yerr3 = np.loadtxt("r3_amp.txt", unpack=True)
 
     P1 = [r1, l, c, v0, rgen, rl] # parametri liberi per i fit
     P2 = [r2, l, c, v0, rgen, rl]
     P3 = [r3, l, c, v0, rgen, rl]
     popt1, pcov1 = curve_fit(amplitude, x1, y1, p0=P1, sigma=yerr1, maxfev=50000) # fit
     popt2, pcov2 = curve_fit(amplitude, x2, y2, p0=P2, sigma=yerr2, maxfev=50000)
-    popt3, pcov3 = curve_fit(amplitude, x3, y3, p0=P3, sigma=yerr3, maxfev=50000)
+#     popt3, pcov3 = curve_fit(amplitude, x3, y3, p0=P3, sigma=yerr3, maxfev=50000)
 
     print("\nR1: ", popt1[0], "\nL: ", popt1[1], "\nC: ", popt1[2], "\nV0: ", popt1[3],
-          "\nRgen: ", popt1[4], "\nRl: ", popt1[5], "\n\n") # stampa parametri dal fit (senza errori)
+          "\nRgen: ", popt1[4], "\nRl: ", popt1[5],
+           "\nX2 / ndf: ", rcs(x1, y1, yerr1, popt1), "\n\n") # stampa parametri dal fit (senza errori)
     print("\nR2: ", popt2[0], "\nL: ", popt2[1], "\nC: ", popt2[2], "\nV0: ", popt2[3],
-          "\nRgen: ", popt2[4], "\nRl: ", popt2[5], "\n\n")
-    print("\nR3: ", popt3[0], "\nL: ", popt3[1], "\nC: ", popt3[2], "\nV0: ", popt3[3],
-          "\nRgen: ", popt3[4], "\nRl: ", popt3[5], "\n\n")
+          "\nRgen: ", popt2[4], "\nRl: ", popt2[5],
+           "\nX2 / ndf: ", rcs(x2, y2, yerr2, popt2), "\n\n")
+#     print("\nR3: ", popt3[0], "\nL: ", popt3[1], "\nC: ", popt3[2], "\nV0: ", popt3[3],
+      #     "\nRgen: ", popt3[4], "\nRl: ", popt3[5], "\n\n")
     
     plt.figure()
     plt.errorbar(x1,y1,yerr=yerr1, linestyle= 'None', color = 'orange')
-    plt.errorbar(x2,y2,yerr=yerr2, linestyle= 'None', color = 'orange')
-    plt.errorbar(x3,y3,yerr=yerr3, linestyle= 'None', color = 'orange')
-    plt.plot(x1, y1, '.', label = "dati sperimentali R1", color = 'orange', markersize=5.0)
-    plt.plot(x2, y2, '.', label = "dati sperimentali R2", color = 'orange', markersize=5.0)
-    plt.plot(x3, y3, '.', label = "dati sperimentali R3", color = 'orange', markersize=5.0)
+    plt.errorbar(x2,y2,yerr=yerr2, linestyle= 'None', color = 'teal')
+#     plt.errorbar(x3,y3,yerr=yerr3, linestyle= 'None', color = 'yellowgreen')
+    plt.plot(x1, y1, '.', label = "dati sperimentali R1", color = 'orange', markersize=7.0)
+    plt.plot(x2, y2, '.', label = "dati sperimentali R2", color = 'teal', markersize=7.0)
+#     plt.plot(x3, y3, '.', label = "dati sperimentali R3", color = 'yellowgreen', markersize=5.0)
     plt.plot(x1, amplitude(x1, *popt1), color = 'red', label = "R1 fit")
     plt.plot(x2, amplitude(x2, *popt2), color = 'blue', label = "R2 fit")
-    plt.plot(x3, amplitude(x3, *popt3), color = 'green', label = "R3 fit")
+#     plt.plot(x3, amplitude(x3, *popt3), color = 'green', label = "R3 fit")
     plt.xlim(100, 4500)
     plt.ylim(0, 6)
     plt.xlabel("frequenza (Hz)", fontsize=20.0)
     plt.ylabel("ampiezza (V)", fontsize=20.0)
-    plt.title("Amplitude fit", fontsize=30.0, fontname='sans-serif')
-    plt.legend(loc='upper left', fontsize=14.0, markerscale=2.0)
+    plt.legend(loc='lower left', fontsize=14.0, markerscale=2.0)
     plt.grid(True)
 
     plt.show()
--- fit_phase.py
+++ fit_phase.py
@@ -17,13 +17,19 @@ def main():
     rl = 120.41
     m = 1.36E-5
 
-    def phase(f, r, l, c, rgen, rl, m): # MANCA FONDO COSTANTE
+    def phase(f, r, l, c, rgen, rl, m):
         w = 2 * np.pi * f
         y = 1 - w**2 * l * c
         rtot = r + rgen
         num = w * (rl**2 * c - y * l)
         den = rl + rtot * (y**2 + (rl * w * c)**2)
         return np.arctan(num / den) + m*f
+    
+    def rcs(x, y , yerr, popt):
+        residuals = y - phase(x, *popt)
+        chi_squared = np.sum(((residuals/yerr)**2))
+        ndof = len(x) - len(popt)
+        return chi_squared / ndof
 
     f1 = 1 / np.sqrt(l * c) / 2 / np.pi # calcolo frequenze di notch e fattori di qualità
     q1 = r1 * c * 2 * np.pi * f1
@@ -36,57 +42,56 @@ def main():
 
     x1,y1,yerr1 = np.loadtxt("r1_phi.txt", unpack=True)
     x2,y2,yerr2 = np.loadtxt("r2_phi.txt", unpack=True)
-    x3,y3,yerr3 = np.loadtxt("r3_phi.txt", unpack=True)
+#     x3,y3,yerr3 = np.loadtxt("r3_phi.txt", unpack=True)
 
     P1 = [r1, l, c, rgen, rl, m] # parametri liberi per i fit
     P2 = [r2, l, c, rgen, rl, m]
     P3 = [r3, l, c, rgen, rl, m]
     popt1, pcov1 = curve_fit(phase, x1, y1, p0=P1, sigma=yerr1, maxfev=50000) # fit
     popt2, pcov2 = curve_fit(phase, x2, y2, p0=P2, sigma=yerr2, maxfev=50000)
-    popt3, pcov3 = curve_fit(phase, x3, y3, p0=P3, sigma=yerr3, maxfev=50000)
+#     popt3, pcov3 = curve_fit(phase, x3, y3, p0=P3, sigma=yerr3, maxfev=50000)
 
     print("\nR1: ", popt1[0], "\nL: ", popt1[1], "\nC: ", popt1[2],
-          "\nRgen: ", popt1[3], "\nRl: ", popt1[4], "\nm: ", popt1[5], "\n\n") # stampa parametri dal fit (senza errori)
+          "\nRgen: ", popt1[3], "\nRl: ", popt1[4], "\nm: ", popt1[5],
+           "\nX2 / ndf: ", rcs(x1, y1, yerr1, popt1), "\n\n") # stampa parametri dal fit (senza errori)
     print("\nR2: ", popt2[0], "\nL: ", popt2[1], "\nC: ", popt2[2],
-          "\nRgen: ", popt2[3], "\nRl: ", popt2[4], "\nm: ", popt2[5], "\n\n")
-    print("\nR3: ", popt3[0], "\nL: ", popt3[1], "\nC: ", popt3[2],
-          "\nRgen: ", popt3[3], "\nRl: ", popt3[4], "\nm: ", popt3[5], "\n\n")
+          "\nRgen: ", popt2[3], "\nRl: ", popt2[4], "\nm: ", popt2[5],
+           "\nX2 / ndf: ", rcs(x2, y2, yerr2, popt2), "\n\n")
+#     print("\nR3: ", popt3[0], "\nL: ", popt3[1], "\nC: ", popt3[2],
+      #     "\nRgen: ", popt3[3], "\nRl: ", popt3[4], "\nm: ", popt3[5], "\n\n")
     
     plt.figure()
     plt.errorbar(x1,y1,yerr=yerr1, linestyle= 'None', color = 'orange')
-    plt.plot(x1,y1, '.', label = "dati sperimentali R1", color = 'orange', markersize=5.0)
+    plt.plot(x1,y1, '.', label = "dati sperimentali R1", color = 'orange', markersize=7.0)
     plt.plot(x1, phase(x1, *popt1), color = 'red', label = "R1 fit")
     plt.xlim(100, 4500)
     plt.ylim(-1.25, 1.25)
     plt.xlabel("frequenza (Hz)", fontsize=20.0)
     plt.ylabel("fase (rad)", fontsize=20.0)
-    plt.title("Phase fit R1", fontsize=30.0, fontname='sans-serif')
     plt.legend(loc='upper left', fontsize=14.0, markerscale=2.0)
     plt.grid(True)
 
     plt.figure()
-    plt.errorbar(x2,y2,yerr=yerr2, linestyle= 'None', color = 'orange')
-    plt.plot(x2,y2, '.', label = "dati sperimentali R2", color = 'orange', markersize=5.0)
+    plt.errorbar(x2,y2,yerr=yerr2, linestyle= 'None', color = 'teal')
+    plt.plot(x2,y2, '.', label = "dati sperimentali R2", color = 'teal', markersize=7.0)
     plt.plot(x2, phase(x2, *popt2), color = 'blue', label = "R2 fit")
     plt.xlim(100, 4500)
     plt.ylim(-1, 1)
     plt.xlabel("frequenza (Hz)", fontsize=20.0)
     plt.ylabel("fase (rad)", fontsize=20.0)
-    plt.title("Phase fit R2", fontsize=30.0, fontname='sans-serif')
     plt.legend(loc='upper left', fontsize=14.0, markerscale=2.0)
     plt.grid(True)
 
-    plt.figure()
-    plt.errorbar(x3,y3,yerr=yerr3, linestyle= 'None', color = 'orange')
-    plt.plot(x3,y3, '.', label = "dati sperimentali R3", color = 'orange', markersize=5.0)
-    plt.plot(x3, phase(x3, *popt3), color = 'green', label = "R3 fit")
-    plt.xlim(100, 4500)
-    plt.ylim(-1, 1)
-    plt.xlabel("frequenza (Hz)", fontsize=20.0)
-    plt.ylabel("fase (rad)", fontsize=20.0)
-    plt.title("Phase fit R3", fontsize=30.0, fontname='sans-serif')
-    plt.legend(loc='upper left', fontsize=14.0, markerscale=2.0)
-    plt.grid(True)
+#     plt.figure()
+#     plt.errorbar(x3,y3,yerr=yerr3, linestyle= 'None', color = 'orange')
+#     plt.plot(x3,y3, '.', label = "dati sperimentali R3", color = 'orange', markersize=7.0)
+#     plt.plot(x3, phase(x3, *popt3), color = 'green', label = "R3 fit")
+#     plt.xlim(100, 4500)
+#     plt.ylim(-1, 1)
+#     plt.xlabel("frequenza (Hz)", fontsize=20.0)
+#     plt.ylabel("fase (rad)", fontsize=20.0)
+#     plt.legend(loc='upper left', fontsize=14.0, markerscale=2.0)
+#     plt.grid(True)
 
     plt.show()
 

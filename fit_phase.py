9eafb141bbbefa8ec1bff68ec70a154ec400d065
Gabriele Amato <amatogabry250@gmail.com>
2024-05-21 10:46:42 +0200

    Removed Rgen from free parameters and fixed bounds at +/- 10%

--- fit_phase.py
+++ fit_phase.py
@@ -17,7 +17,7 @@ def main():
     rl = 120.41
     m = 1.36E-5
 
-    def phase(f, r, l, c, m, rl, rgen):
+    def phase(f, r, l, c, m, rl):
         w = 2 * np.pi * f
         y = 1 - w**2 * l * c
         rtot = r + rgen
@@ -44,18 +44,18 @@ def main():
     x2,y2,yerr2 = np.loadtxt("r2_phi.txt", unpack=True)
 #     x3,y3,yerr3 = np.loadtxt("r3_phi.txt", unpack=True)
 
-    P1 = [r1, l, c, m, rl, rgen] # parametri liberi per i fit
-    P2 = [r2, l, c, m, rl, rgen]
-    P3 = [r3, l, c, m, rl, rgen]
+    P1 = [r1, l, c, m, rl] # parametri liberi per i fit
+    P2 = [r2, l, c, m, rl]
+    P3 = [r3, l, c, m, rl]
     popt1, pcov1 = curve_fit(phase, x1, y1, p0=P1, sigma=yerr1,
-                             bounds=([r1 - 200, l - 1E-2, c - 1E-5, m - 1E-3, rl - 50, rgen - 20], [r1 + 200, l + 1E-2, c + 1E-5, m + 1E-3, rl + 50, rgen + 20]), maxfev=50000) # fit
+                             bounds=([r1 - 80, l - 5E-3, c - 1E-8, m - 1E-6, rl - 15], [r1 + 80, l + 5E-3, c + 1E-8, m + 2E-6, rl + 15]), maxfev=50000) # fit
     popt2, pcov2 = curve_fit(phase, x2, y2, p0=P2, sigma=yerr2,
-                             bounds=([r2 - 500, l - 1E-2, c - 1E-5, m - 1E-3, rl - 50, rgen - 20], [r2 + 500, l + 1E-2, c + 1E-5, m + 1E-3, rl + 50, rgen + 20]), maxfev=50000)
+                             bounds=([r2 - 270, l - 5E-3, c - 1E-8, m - 1E-6, rl - 15], [r2 + 270, l + 5E-3, c + 1E-5, m + 2E-6, rl + 15]), maxfev=50000)
 #     popt3, pcov3 = curve_fit(phase, x3, y3, p0=P3, sigma=yerr3, maxfev=50000)
 
-    print("\nR1: ", popt1[0], "\nL: ", popt1[1], "\nC: ", popt1[2], "\nm: ", popt1[3], "\nRl: ", popt1[4], "\nRgen: ", popt1[5],
+    print("\nR1: ", popt1[0], "\nL: ", popt1[1], "\nC: ", popt1[2], "\nm: ", popt1[3], "\nRl: ", popt1[4],
            "\nX2 / ndf: ", rcs(x1, y1, yerr1, popt1), "\n\n") # stampa parametri dal fit (senza errori)
-    print("\nR2: ", popt2[0], "\nL: ", popt2[1], "\nC: ", popt2[2], "\nm: ", popt2[3], "\nRl: ", popt1[4], "\nRgen: ", popt1[5],
+    print("\nR2: ", popt2[0], "\nL: ", popt2[1], "\nC: ", popt2[2], "\nm: ", popt2[3], "\nRl: ", popt1[4],
            "\nX2 / ndf: ", rcs(x2, y2, yerr2, popt2), "\n\n")
 #     print("\nR3: ", popt3[0], "\nL: ", popt3[1], "\nC: ", popt3[2],
       #     "\nRgen: ", popt3[3], "\nRl: ", popt3[4], "\nm: ", popt3[5], "\n\n")
